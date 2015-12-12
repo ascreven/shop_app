8b09ce6a9a74d610935f5022172b9fa845fdcb49
Alexandra <akscreven@gmail.com>
2015-12-12 16:21:59 -0500

    added clothes controller and index route

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root to: 'clothes#index'
+  get "/clothes", to: "clothes#index"
+  resources :clothes
+
 end
